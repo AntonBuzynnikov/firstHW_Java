4f64705349bbc3e1d6819a4037fa28f8fadf569a
Buzynnikov <buzynnikov2111@gmail.com>
2023-06-08 19:28:13 +0500

    Complete 003

---
+++ Example003/Example.java
@@ -0,0 +1,25 @@
+package FirstHW.Example003;
+
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите имя: ");
+            String name = scan.nextLine();
+            String welcome = "";
+            LocalTime currentTime = LocalTime.now();
+            if(currentTime.isAfter(LocalTime.parse("23:59:59.999999999"))){
+                welcome = "Доброй ночи, " + name;
+            } else if(currentTime.isAfter(LocalTime.parse("18:00:00.000000000"))){
+                welcome = "Добрый вечер, " + name;
+            } else if(currentTime.isAfter(LocalTime.parse("12:00:00.000000000"))){
+                welcome = "Добрый день, " + name;
+            } else if(currentTime.isAfter(LocalTime.parse("05:00:00.000000000"))){
+                welcome = "Доброе утро, " + name;
+            }
+            System.out.println(welcome);
+        
+    }
+}
